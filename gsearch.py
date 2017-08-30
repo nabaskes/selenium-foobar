94c62bd2bf2430f5ced7f1f37b6acad9ccbecc5b
nabaskes <nabaskes@uchicago.edu>
2017-08-29 22:48:23 -0400

    we can log into google and pound them with searches.  they will captcha me after about 50 so i need to figure out a way to continue after solving them, or to get captchad less

--- gsearch.py
+++ gsearch.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from config import Config
 
 driver = webdriver.Firefox()
 driver.get("http://www.google.com")
@@ -13,12 +15,34 @@ if not searches:
     searches.append("here comes dat boi")
     searches.append("The mighty King Cobra, King of the Jungle")
 
-inputElement = driver.find_element_by_name("q")
+driver.get("https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F%3Fgws_rd%3Dssl&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
+inputElement = driver.find_element_by_name("identifier")
+inputElement.send_keys(Config.user)
+inputElement = driver.find_element_by_id("identifierNext")
+inputElement.click()
+WebDriverWait(driver,3000).until(EC.presence_of_element_located((By.ID,"password")))
+inputElement = driver.find_element_by_name("password")
+inputElement.send_keys(Config.pw)
+WebDriverWait(driver,3000).until(EC.presence_of_element_located((By.ID,"passwordNext")))
+inputElement = driver.find_element_by_id("passwordNext")
+inputElement.click()
+WebDriverWait(driver,10).until(EC.presence_of_element_located((By.NAME,"q")))
+
+#you should be redirected to google homepage
 
 for item in searches:
     print(item)
+    inputElement = driver.find_element_by_name("q").clear()
+    inputElement = driver.find_element_by_name("q")
     inputElement.send_keys(item)
+    inputElement.submit()
     WebDriverWait(driver, 10).until(EC.title_contains(item))
-    WebDriverWait(driver, 10)
+    try:
+        ie = driver.find_element_by_xpath("//*[contains(text(),'foobar')]")
+        break
+    except:
+        pass
+    
+    WebDriverWait(driver, 30)
 
 #driver.quit()
